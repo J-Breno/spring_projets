4953a175f4f28e8867e261f2598b03455d1dc515
J-Breno <joao.breno85@hotmail.com>
2025-03-11 18:06:56 -0300

    criando DTO e estruturando camadas

---
+++ professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.github.JBreno.dscommerce.services;
+
+import com.github.JBreno.dscommerce.dto.ProductDTO;
+import com.github.JBreno.dscommerce.entities.Product;
+import com.github.JBreno.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+
+}
