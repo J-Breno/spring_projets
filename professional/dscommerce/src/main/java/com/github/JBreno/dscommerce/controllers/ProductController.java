cb260205b9a82dd4856299624fc1223b2a923e8c
J-Breno <joao.breno85@hotmail.com>
2025-03-11 23:09:31 -0300

    Busca paginada de produtos

--- professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/controllers/ProductController.java
+++ professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/controllers/ProductController.java
@@ -5,12 +5,15 @@ import com.github.JBreno.dscommerce.entities.Product;
 import com.github.JBreno.dscommerce.repositories.ProductRepository;
 import com.github.JBreno.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,4 +29,9 @@ public class ProductController {
        return dto;
     }
 
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
+    }
 }
--- professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/services/ProductService.java
+++ professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.github.JBreno.dscommerce.dto.ProductDTO;
 import com.github.JBreno.dscommerce.entities.Product;
 import com.github.JBreno.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,4 +24,9 @@ public class ProductService {
         return new ProductDTO(product);
     }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
 }
