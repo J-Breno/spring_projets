d534633055a5819c80de811be5fc044381b77b69
J-Breno <joao.breno85@hotmail.com>
2025-03-13 09:30:08 -0300

    Validação com Bean Validation

--- professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/dto/ProductDTO.java
+++ professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,18 @@
 package com.github.JBreno.dscommerce.dto;
 
 import com.github.JBreno.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 3, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
