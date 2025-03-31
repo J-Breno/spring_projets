df0eca751292dee00dc2ec870dc1b9e349eeb15f
J-Breno <joao.breno85@hotmail.com>
2025-03-31 13:30:05 -0300

    Fix validation

--- professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/dto/ProductDTO.java
+++ professional/dscommerce/src/main/java/com/github/JBreno/dscommerce/dto/ProductDTO.java
@@ -16,6 +16,7 @@ public class ProductDTO {
     @Size(min = 3, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
+    @NotNull(message = "campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
