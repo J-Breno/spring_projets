52b190d9cd808e4a0656ab6a96c7c6f180827716
J-Breno <joao.breno85@hotmail.com>
2025-04-02 22:28:42 -0300

    Teste saque total deveria limpar saldo e retornar todo saldo

--- expert/junit/aula-junit/aula1/src/entities/Account.java
+++ expert/junit/aula-junit/aula1/src/entities/Account.java
@@ -39,7 +39,7 @@ public class Account {
         balance -= amount;
     }
 
-    public double fullWithdraw(Double amount) {
+    public double fullWithdraw() {
         double aux = balance;
         balance = 0.0;
         return aux;
--- expert/junit/aula-junit/aula1/src/tests/entities/AccountTests.java
+++ expert/junit/aula-junit/aula1/src/tests/entities/AccountTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 	
@@ -11,7 +12,7 @@ public class AccountTests {
 	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.createEmptyAccount();
 		
 		acc.deposit(amount);
 		
@@ -21,11 +22,23 @@ public class AccountTests {
 	@Test
 	public void depositShouldDoNothingWhenNegativeAmount() {
 		double expectedValue = 100.0;
-		Account acc = new Account(1L, expectedValue);
+		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = -200.00;
 		
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
 	}
 }
