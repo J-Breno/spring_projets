22429d9a9babce7effd60a54047e452ebdfb346c
J-Breno <joao.breno85@hotmail.com>
2025-04-07 04:32:23 -0300

    Basic Validation

--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/dto/ProductDTO.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/dto/ProductDTO.java
@@ -1,19 +1,29 @@
 package com.github.Jbreno.dscatalog.dto;
 
-import com.github.Jbreno.dscatalog.entities.Category;
-import com.github.Jbreno.dscatalog.entities.Product;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import com.github.Jbreno.dscatalog.entities.Category;
+import com.github.Jbreno.dscatalog.entities.Product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
     private Long id;
+    @Size(min = 5, max = 60, message = "Nome deve ter entre 5 a 60 caracteres")
+	@NotBlank(message = "Campo obrigatório")
     private String name;
+	@NotBlank(message = "Campo obrigatório")
     private String description;
+    @Positive(message = "Preço deve ser um valor positivo")
     private Double price;
     private String imgUrl;
+    @PastOrPresent(message = "A data do produto não pode ser futura")
     private Instant date;
 
     List<CategoryDTO> categories = new ArrayList<>();
--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/resources/ProductResource.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/resources/ProductResource.java
@@ -1,17 +1,25 @@
 package com.github.Jbreno.dscatalog.resources;
 
-import com.github.Jbreno.dscatalog.dto.ProductDTO;
-import com.github.Jbreno.dscatalog.services.ProductService;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import com.github.Jbreno.dscatalog.dto.ProductDTO;
+import com.github.Jbreno.dscatalog.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -33,7 +41,7 @@ public class ProductResource {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(dto.getId()).toUri();
@@ -41,7 +49,7 @@ public class ProductResource {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update( @PathVariable Long id, @RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> update( @PathVariable Long id,@Valid @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
