1e92fd2f2e140a7a3585a208627126042aa5f9a8
J-Breno <joao.breno85@hotmail.com>
2025-04-07 05:25:59 -0300

    Custom ConstraintValidator

--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/dto/UserInsertDTO.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.github.Jbreno.dscatalog.dto;
 
+import com.github.Jbreno.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private String password;
 	
--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import com.github.Jbreno.dscatalog.services.exceptions.ResourceNotFoundException
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -38,14 +39,19 @@ public class ResourceExceptionHandler {
     }
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-        StandardError error = new StandardError();
+        ValidationError error = new ValidationError();
         error.setTimestamp(Instant.now());
         error.setStatus(status.value());
         error.setError("Validation exception");
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
+        
+        for(FieldError f : e.getBindingResult().getFieldErrors()) {
+        	error.addError(f.getField(), f.getDefaultMessage());
+        }
+        
         return ResponseEntity.status(status).body(error);
     }
 }
