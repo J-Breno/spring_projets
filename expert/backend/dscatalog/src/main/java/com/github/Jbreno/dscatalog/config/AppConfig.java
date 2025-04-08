a6b7dab22c86a8c8305cd1168547dadfb687c87f
J-Breno <joao.breno85@hotmail.com>
2025-04-08 11:33:36 -0300

    Checkilist do Spring OAuth2

--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/config/AppConfig.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/config/AppConfig.java
@@ -2,8 +2,11 @@ package com.github.Jbreno.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class AppConfig {
@@ -12,4 +15,17 @@ public class AppConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .anyRequest().authenticated()
+            )
+            .headers(headers -> headers.frameOptions().disable()) // Habilita o uso de frames para o H2
+            .csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))); // Desativa CSRF para o H2
+
+        return http.build();
+    }
 }
