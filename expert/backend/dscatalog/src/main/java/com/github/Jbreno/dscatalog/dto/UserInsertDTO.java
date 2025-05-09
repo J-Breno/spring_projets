8df061a3ffc4cc29d361f15e6154ff48416cdf6a
J-Breno <joao.breno85@hotmail.com>
2025-05-08 23:29:24 -0300

    Configurando senha de app do Gmail

--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/dto/UserInsertDTO.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/dto/UserInsertDTO.java
@@ -1,9 +1,14 @@
 package com.github.Jbreno.dscatalog.dto;
 
 import com.github.Jbreno.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO{
+	@NotBlank(message = "Campo obrigatório")
+	@Size(min = 8, message = "Deve ter no mínimo 8 carcteres")
+
 	private String password;
 	
 	public UserInsertDTO() {
--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/repositories/RoleRepository.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/repositories/RoleRepository.java
@@ -7,4 +7,5 @@ import com.github.Jbreno.dscatalog.entities.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByAuthority(String authority);
 }
