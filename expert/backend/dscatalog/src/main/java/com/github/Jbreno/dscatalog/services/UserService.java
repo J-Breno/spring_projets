3863570404566b1dbd403742853292c9263e1a9c
J-Breno <joao.breno85@hotmail.com>
2025-05-09 03:19:39 -0300

    Obtendo usuário logado

--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/services/UserService.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/services/UserService.java
@@ -40,6 +40,8 @@ public class UserService implements UserDetailsService{
     
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private AuthService authService;
 
     @Transactional(readOnly = true)
     public Page<UserDTO> findAllPage(Pageable pageable) {
@@ -51,6 +53,12 @@ public class UserService implements UserDetailsService{
     public UserDTO findById(Long id) {
         Optional<User> obj = repository.findById(id);
         User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new UserDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findMe() {
+        User entity = authService.authenticated();
         return new UserDTO(entity);
     }
 
