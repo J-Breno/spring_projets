9e28ec8a03c8a2153813eaac3eab7271268607ba
J-Breno <joao.breno85@hotmail.com>
2025-05-09 02:36:06 -0300

    Implementando a lógico de envio do token por email

--- expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/services/AuthService.java
+++ expert/backend/dscatalog/src/main/java/com/github/Jbreno/dscatalog/services/AuthService.java
@@ -1,12 +1,55 @@
 package com.github.Jbreno.dscatalog.services;
 
 import com.github.Jbreno.dscatalog.dto.EmailDTO;
-import jakarta.validation.Valid;
+import com.github.Jbreno.dscatalog.entities.PasswordRecover;
+import com.github.Jbreno.dscatalog.entities.User;
+import com.github.Jbreno.dscatalog.repositories.PasswordRecoverRepository;
+import com.github.Jbreno.dscatalog.repositories.UserRepository;
+import com.github.Jbreno.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinutes;
+
+    @Value("${email.password-recover.uri}")
+    private String recoverUri;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Transactional
     public void createRecoverToken(EmailDTO body) {
+        User user = userRepository.findByEmail(body.getEmail());
+        if (user == null) {
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
+
+        String token = UUID.randomUUID().toString();
+
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(body.getEmail());
+        entity.setToken(token);
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60L));
+        entity = passwordRecoverRepository.save(entity);
+
+        String text = "Acesse o link para definir uma nova senha\n\n "
+            + recoverUri + token + ". Validade de " + tokenMinutes + " minutos";
+
+        emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
     }
 }
