b0611e01e9246d348d8fbab6e2533a6f1d12273a
J-Breno <joao.breno85@hotmail.com>
2025-05-09 05:42:11 -0300

    Fazendo desafio MovieFlix caso de uso

---
+++ expert/desafios/Desafio Movieflix Casos de Uso/src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -0,0 +1,35 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @PreAuthorize("hasAnyRole('ROLE_VISITOR','ROLE_MEMBER')")
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDetailsDTO> findById(@PathVariable Long id) {
+        MovieDetailsDTO movieDetailsDTO = movieService.findById(id);
+        return ResponseEntity.ok().body(movieDetailsDTO);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_VISITOR','ROLE_MEMBER')")
+    @GetMapping
+    public ResponseEntity<Page<MovieCardDTO>> findByGenre(
+            @RequestParam(name = "genreId", required = false) Long genreId,
+            Pageable pageable) {
+        Page<MovieCardDTO> result = movieService.findByGenre(genreId, pageable);
+        return ResponseEntity.ok(result);
+    }
+}
