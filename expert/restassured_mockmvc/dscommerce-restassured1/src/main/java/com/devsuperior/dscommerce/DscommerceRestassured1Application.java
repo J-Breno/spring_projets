62383940de60f29182e74ead0b0481b3d90b17f6
J-Breno <joao.breno85@hotmail.com>
2025-05-14 14:12:12 -0300

    Inclusão dependencias RestAssured

---
+++ expert/restassured_mockmvc/dscommerce-restassured1/src/main/java/com/devsuperior/dscommerce/DscommerceRestassured1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceRestassured1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceRestassured1Application.class, args);
+	}
+
+}
