5fc4c08f3644b5becd217aaeabad6bb8b3955e3a
J-Breno <joao.breno85@hotmail.com>
2025-05-14 15:58:08 -0300

    Test busca de produto por nome

--- expert/restassured_mockmvc/dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ expert/restassured_mockmvc/dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     public void setUp() throws Exception {
         baseURI = "http://localhost:8080";
+        productName = "Macbook";
     }
 
     @Test
@@ -27,7 +29,36 @@ public class ProductControllerRA {
                 .body("name", equalTo("Smart TV"))
                 .body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
                 .body("price", is(2190.0F))
-                .body("categories", hasItems(2, 3))
+                .body("categories.id", hasItems(2, 3))
                 .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsEmpty() throws Exception {
+        given()
+                .get("/products?page=0")
+                .then()
+                .statusCode(200)
+                .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldResturnPageProductsWhenProductNameIsNotEmpty() throws Exception {
+        given()
+                .get("/products?name={productName}", productName)
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(3))
+                .body("content.name[0]", equalTo("Macbook Pro"))
+                .body("content.price[0]", is(1250.0F));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() throws Exception {
+        given()
+                .get("/products?size=25")
+                .then()
+                        .statusCode(200)
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
     }
 }
