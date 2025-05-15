b1207d14eb0b19a9416de59f62f47ea33d8eda73
J-Breno <joao.breno85@hotmail.com>
2025-05-15 01:18:48 -0300

    User, Role

---
+++ reference/dslearn/src/test/java/com/github/jbreno/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbreno.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
