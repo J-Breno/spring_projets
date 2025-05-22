66f2fb867a5c8ba036e887ab713315d4a2b1ee12
J-Breno <joao.breno85@hotmail.com>
2025-05-22 02:34:38 -0300

    GET Post by id

---
+++ reference/mongodb/workshopmongo/src/main/java/com/github/jbreno/workshopmongo/controllers/PostController.java
@@ -0,0 +1,27 @@
+package com.github.jbreno.workshopmongo.controllers;
+
+import com.github.jbreno.workshopmongo.models.dto.PostDTO;
+import com.github.jbreno.workshopmongo.models.dto.PostDTO;
+import com.github.jbreno.workshopmongo.services.PostService;
+import com.github.jbreno.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<PostDTO> findById(@PathVariable String id) {
+        PostDTO postDTO = postService.findById(id);
+        return ResponseEntity.ok(postDTO);
+    }
+}
---
+++ reference/mongodb/workshopmongo/src/main/java/com/github/jbreno/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.github.jbreno.workshopmongo.services;
+
+import com.github.jbreno.workshopmongo.models.dto.PostDTO;
+import com.github.jbreno.workshopmongo.models.dto.PostDTO;
+import com.github.jbreno.workshopmongo.models.entities.Post;
+import com.github.jbreno.workshopmongo.repositories.PostRepository;
+import com.github.jbreno.workshopmongo.repositories.PostRepository;
+import com.github.jbreno.workshopmongo.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public PostDTO findById(String id) {
+        Post post = getEntityById(id);
+
+        return new PostDTO(post);
+    }
+
+    private Post getEntityById(String id) {
+        return postRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("Usuário não encontrado");
+        });
+    }
+}
