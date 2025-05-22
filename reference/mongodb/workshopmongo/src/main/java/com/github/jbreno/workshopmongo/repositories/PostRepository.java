6954e09dfe975981b6955078240b4944d6018ad7
J-Breno <joao.breno85@hotmail.com>
2025-05-22 02:50:45 -0300

    GET Post by title, query operators

--- reference/mongodb/workshopmongo/src/main/java/com/github/jbreno/workshopmongo/repositories/PostRepository.java
+++ reference/mongodb/workshopmongo/src/main/java/com/github/jbreno/workshopmongo/repositories/PostRepository.java
@@ -2,11 +2,16 @@ package com.github.jbreno.workshopmongo.repositories;
 
 import com.github.jbreno.workshopmongo.models.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+    List<Post> searchTitle(String title);
+
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
